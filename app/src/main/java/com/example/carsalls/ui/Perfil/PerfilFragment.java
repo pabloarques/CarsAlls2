bea1a7dfd49102eb36acbaff38a525ffcdef7666
pabarqlop <pabloarques98@gmail.com>
2023-01-09 11:25:07 +0100

    9/1/23

--- app/src/main/java/com/example/carsalls/ui/Perfil/PerfilFragment.java
+++ app/src/main/java/com/example/carsalls/ui/Perfil/PerfilFragment.java
@@ -1,25 +1,44 @@
 package com.example.carsalls.ui.Perfil;
 
+import static android.app.Activity.RESULT_OK;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.carsalls.ViewModels.SharedViewModel;
 import com.example.carsalls.databinding.FragmentPerfilBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
 public class PerfilFragment extends Fragment {
 
     private FragmentPerfilBinding binding;
+    private FirebaseUser authUser;
+    private FirebaseAuth mAuth;
+
     private static final int COD_SEL_IMAGE = 300;
+
+    StorageReference storageReference;
+    String storage_path = "Perfil/*";
+
     private Uri image_url;
     String photo = "photo";
+    String idd;
 
+    ProgressDialog progressDialog;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -29,14 +48,46 @@ public class PerfilFragment extends Fragment {
         binding = FragmentPerfilBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        StorageReference storageReference;
-        String storage_path = "Perfil/*";
+        SharedViewModel sharedViewModel = new ViewModelProvider(requireActivity()).get(SharedViewModel.class);
+        sharedViewModel.getUser().observe(getViewLifecycleOwner(), (user) -> {
+                authUser = user;
 
+                storageReference = FirebaseStorage.getInstance().getReference();
 
+        });
+    
+        binding.btnEditar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                actualizarFoto();
+            }
+        });
 
+        binding.btnEliminar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        
 
         return root;
     }
+
+    private void actualizarFoto() {
+        Intent i = new Intent(Intent.ACTION_PICK);
+        i.setType("image/*");
+
+        startActivityForResult(i, COD_SEL_IMAGE);
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        Log.d("image_url", "requestCode - RESULT_OK: " +requestCode + " "+ RESULT_OK);
+    }
+
+
 
     @Override
     public void onDestroyView() {
